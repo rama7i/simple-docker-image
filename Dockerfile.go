582541bdc8c1c26b9fa639c52f9f9d6777b15b57
rama7i <diaa@lamsaworld.com>
2021-11-10 11:54:34 +0400

    cv
    cb

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,3 +1,4 @@
+FROM 694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service:server-develop358 AS builder
 FROM alpine:latest AS webapp-server
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
