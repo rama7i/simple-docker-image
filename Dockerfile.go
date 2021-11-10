322512b1e6c1d1948fbf22320f6440c538521db8
rama7i <diaa@lamsaworld.com>
2021-11-10 12:36:50 +0400

    xcv
    cxv

--- Dockerfile.go
+++ Dockerfile.go
@@ -3,7 +3,7 @@ WORKDIR /go/src/github.com/alexellis/href-counter/
 RUN go get -d -v golang.org/x/net/html  
 COPY app.go    .
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
-FROM alpine:latest AS webapp-server
+FROM alpine:latest AS webapp
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 COPY --from=gobuilder /go/src/github.com/alexellis/href-counter/app .
