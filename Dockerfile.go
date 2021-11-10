136af9d68e39ba7f4aca6f1d905b7375baad4910
rama7i <diaa@lamsaworld.com>
2021-11-10 17:22:52 +0400

    sddf
    dfsd

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,4 +1,11 @@
+FROM golang:1.7.3 AS gobuilder
+WORKDIR /go/src/github.com/alexellis/href-counter/
+RUN go get -d -v golang.org/x/net/html  
+COPY app.go    .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
+
 FROM alpine:latest AS webapp
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
+COPY --from='694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service/cache' /go/src/github.com/alexellis/href-counter/app .
 CMD ["./app"]
