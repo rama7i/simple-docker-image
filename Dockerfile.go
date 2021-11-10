5f16c1c4d22abefe6298f46578afd483047f4a2c
rama7i <diaa@lamsaworld.com>
2021-11-10 17:26:28 +0400

    xv
    cg

--- Dockerfile.go
+++ Dockerfile.go
@@ -7,5 +7,5 @@ RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
 FROM alpine:latest AS webapp
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
-COPY --from='694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service/cache' /go/src/github.com/alexellis/href-counter/app .
+COPY --from='694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service/cache:c84bf959d8254461502af741bcbb59c33af39272dfc54a39c8cccff806524562' /go/src/github.com/alexellis/href-counter/app .
 CMD ["./app"]
