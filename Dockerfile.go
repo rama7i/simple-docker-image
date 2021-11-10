058010f1d453f142a8fdf6ccb3c1008eb58e8423
rama7i <diaa@lamsaworld.com>
2021-11-10 17:28:09 +0400

    dvdf
    dffg

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,9 +1,3 @@
-FROM golang:1.7.3 AS gobuilder
-WORKDIR /go/src/github.com/alexellis/href-counter/
-RUN go get -d -v golang.org/x/net/html  
-COPY app.go    .
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
-
 FROM alpine:latest AS webapp
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
