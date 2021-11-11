9f26cd8e0c92fb89b6541408bbf8aca910c07537
rama7i <diaa@lamsaworld.com>
2021-11-11 12:10:23 +0400

    asd
    as

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,4 +1,4 @@
-FROM 694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service/cache:c84bf959d8254461502af741bcbb59c33af39272dfc54a39c8cccff806524562 AS gobuilder
+FROM 694770204095.dkr.ecr.us-east-1.amazonaws.com/webapp-service/cache AS gobuilder
 FROM alpine:latest AS webapp
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
